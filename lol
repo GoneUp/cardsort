b76d079170e6dca58b6fc1cccae1f1a3dde23226
hstrobel <GoneUp@users.noreply.github.com>
2025-09-19 20:47:04 +0200

    init

---
+++ lol
@@ -0,0 +1,38 @@
+LOL
+
+Intro
+
+
+Magzin muss sich bewegen -> aktor
+Karte aktor
+Camera feed in 
+Steuerung? 
+ 
+
+Display Segmentanzeige
+
+Größe 200 130 
+
+Diskussion Magzinführung
+-> Unterbau, plus von oben nach unten wäre gut
+--
+Startposition, fährt neu
+
+--
+Wie fährt das Magazin?
+Zahnstange plus Motor Zahnstangengetriebe
+Elektrischen Zylinder plus Spindel
+
+--
+Netzteil für Motor Driver + Raspi
+
+Todo 
+
+
+Masse bewegeung, geschwindigkeit 
+Masse + Leistung
+Linearmotor
+Zahnstange Spindel
+
+TODO 
+Raspi + Camera
---
+++ poc.py
@@ -0,0 +1,43 @@
+import RPi.GPIO as GPIO
+import time
+
+# Pin configuration
+X_STEP = 17
+X_DIR = 27
+Z_STEP = 24
+Z_DIR = 25
+EN = 4
+
+# Setup
+GPIO.setmode(GPIO.BCM)
+GPIO.setup([X_STEP, X_DIR, Z_STEP, Z_DIR, EN], GPIO.OUT)
+
+# Enable drivers (LOW = enabled)
+GPIO.output(EN, GPIO.LOW)
+
+
+print("enable done")
+# Move X Axis
+GPIO.output(X_DIR, GPIO.HIGH)  # or GPIO.LOW to reverse
+for _ in range(2000):  # 200 steps = ~1 revolution
+    GPIO.output(X_STEP, GPIO.HIGH)
+    time.sleep(0.008)
+    GPIO.output(X_STEP, GPIO.LOW)
+    time.sleep(0.008)
+
+print("x done")
+# Short delay between axis moves
+time.sleep(0.5)
+
+# Move Z Axis
+GPIO.output(Z_DIR, GPIO.HIGH)  # or GPIO.HIGH
+for _ in range(2000):
+    GPIO.output(Z_STEP, GPIO.HIGH)
+    time.sleep(0.001)
+    GPIO.output(Z_STEP, GPIO.LOW)
+    time.sleep(0.001)
+
+print("z done");
+# Disable drivers (optional)
+GPIO.output(EN, GPIO.HIGH)
+GPIO.cleanup()
